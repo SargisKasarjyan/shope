4dcf28853bcc731efaa556bb337da7d97a4fe655
SargisKasarjyan <105929936+SargisKasarjyan@users.noreply.github.com>
2022-11-14 21:14:16 +0400

    Shope

---
+++ label.js
@@ -0,0 +1,710 @@
+const { createElement } = require("react/cjs/react.production.min")
+
+dax = document.getElementById("dv2")
+nkar = document.getElementById("dv4")
+aj = document.getElementById("dv3")
+
+var arr = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar.style.backgroundImage = "url(img/1.jpg)"
+nkar.style.backgroundSize = "cover"
+var l = 0
+
+aj.onclick = function () {
+    l++
+    nkar.style.backgroundImage = `url(img/${arr[l]})`
+    if (l === 4) {
+        l = 0
+        nkar.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax.onclick = function () {
+    l--
+    nkar.style.backgroundImage = `url(img/${arr[l]})`
+    if (l < 0) {
+        l = 3
+        nkar.style.backgroundImage = `url(img/${arr[l]})`
+    }
+}
+dax2 = document.getElementById("dv2_2")
+nkar2 = document.getElementById("dv4_2")
+aj2 = document.getElementById("dv3_2")
+
+var arr2 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar2.style.backgroundImage = "url(img/1.jpg)"
+nkar2.style.backgroundSize = "cover"
+var l = 0
+
+aj2.onclick = function () {
+    l++
+    nkar2.style.backgroundImage = `url(img/${arr2[l]})`
+    if (l === 4) {
+        l = 0
+        nkar2.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax2.onclick = function () {
+    l--
+    nkar2.style.backgroundImage = `url(img/${arr2[l]})`
+    if (l < 0) {
+        l = 3
+        nkar2.style.backgroundImage = `url(img/${arr2[l]})`
+    }
+}
+
+dax3 = document.getElementById("dv2_3")
+nkar3 = document.getElementById("dv4_3")
+aj3 = document.getElementById("dv3_3")
+
+var arr3 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar3.style.backgroundImage = "url(img/1.jpg)"
+nkar3.style.backgroundSize = "cover"
+var l = 0
+
+aj3.onclick = function () {
+    l++
+    nkar3.style.backgroundImage = `url(img/${arr3[l]})`
+    if (l === 4) {
+        l = 0
+        nkar3.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax3.onclick = function () {
+    l--
+    nkar3.style.backgroundImage = `url(img/${arr3[l]})`
+    if (l < 0) {
+        l = 3
+        nkar3.style.backgroundImage = `url(img/${arr3[l]})`
+    }
+}
+dax4 = document.getElementById("dv2_4")
+nkar4 = document.getElementById("dv4_4")
+aj4 = document.getElementById("dv3_4")
+
+var arr4 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar4.style.backgroundImage = "url(img/1.jpg)"
+nkar4.style.backgroundSize = "cover"
+var l = 0
+
+aj4.onclick = function () {
+    l++
+    nkar4.style.backgroundImage = `url(img/${arr4[l]})`
+    if (l === 4) {
+        l = 0
+        nkar4.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax4.onclick = function () {
+    l--
+    nkar4.style.backgroundImage = `url(img/${arr4[l]})`
+    if (l < 0) {
+        l = 3
+        nkar4.style.backgroundImage = `url(img/${arr4[l]})`
+    }
+}
+dax5 = document.getElementById("dv2_5")
+nkar5 = document.getElementById("dv4_5")
+aj5 = document.getElementById("dv3_5")
+
+var arr5 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar5.style.backgroundImage = "url(img/1.jpg)"
+nkar5.style.backgroundSize = "cover"
+var l = 0
+
+aj5.onclick = function () {
+    l++
+    nkar5.style.backgroundImage = `url(img/${arr5[l]})`
+    if (l === 4) {
+        l = 0
+        nkar5.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax5.onclick = function () {
+    l--
+    nkar5.style.backgroundImage = `url(img/${arr5[l]})`
+    if (l < 0) {
+        l = 3
+        nkar5.style.backgroundImage = `url(img/${arr5[l]})`
+    }
+}
+dax6 = document.getElementById("dv2_6")
+nkar6 = document.getElementById("dv4_6")
+aj6 = document.getElementById("dv3_6")
+
+var arr6 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar6.style.backgroundImage = "url(img/1.jpg)"
+nkar6.style.backgroundSize = "cover"
+var l = 0
+
+aj6.onclick = function () {
+    l++
+    nkar6.style.backgroundImage = `url(img/${arr6[l]})`
+    if (l === 4) {
+        l = 0
+        nkar6.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax6.onclick = function () {
+    l--
+    nkar6.style.backgroundImage = `url(img/${arr6[l]})`
+    if (l < 0) {
+        l = 3
+        nkar6.style.backgroundImage = `url(img/${arr6[l]})`
+    }
+}
+dax7 = document.getElementById("dv2_7")
+nkar7 = document.getElementById("dv4_7")
+aj7 = document.getElementById("dv3_7")
+
+var arr7 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar7.style.backgroundImage = "url(img/1.jpg)"
+nkar7.style.backgroundSize = "cover"
+var l = 0
+
+aj7.onclick = function () {
+    l++
+    nkar7.style.backgroundImage = `url(img/${arr7[l]})`
+    if (l === 4) {
+        l = 0
+        nkar7.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax7.onclick = function () {
+    l--
+    nkar7.style.backgroundImage = `url(img/${arr7[l]})`
+    if (l < 0) {
+        l = 3
+        nkar7.style.backgroundImage = `url(img/${arr7[l]})`
+    }
+}
+dax8 = document.getElementById("dv2_8")
+nkar8 = document.getElementById("dv4_8")
+aj8 = document.getElementById("dv3_8")
+
+var arr8 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar8.style.backgroundImage = "url(img/1.jpg)"
+nkar8.style.backgroundSize = "cover"
+var l = 0
+
+aj8.onclick = function () {
+    l++
+    nkar8.style.backgroundImage = `url(img/${arr8[l]})`
+    if (l === 4) {
+        l = 0
+        nkar8.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax8.onclick = function () {
+    l--
+    nkar8.style.backgroundImage = `url(img/${arr8[l]})`
+    if (l < 0) {
+        l = 3
+        nkar8.style.backgroundImage = `url(img/${arr8[l]})`
+    }
+}
+dax9 = document.getElementById("dv2_9")
+nkar9 = document.getElementById("dv4_9")
+aj9 = document.getElementById("dv3_9")
+
+var arr9 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar9.style.backgroundImage = "url(img/1.jpg)"
+nkar9.style.backgroundSize = "cover"
+var l = 0
+
+aj9.onclick = function () {
+    l++
+    nkar9.style.backgroundImage = `url(img/${arr9[l]})`
+    if (l === 4) {
+        l = 0
+        nkar9.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax9.onclick = function () {
+    l--
+    nkar9.style.backgroundImage = `url(img/${arr9[l]})`
+    if (l < 0) {
+        l = 3
+        nkar9.style.backgroundImage = `url(img/${arr9[l]})`
+    }
+}
+//gdsftterityewouryweoyrfioeyiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+dax10 = document.getElementById("dv3_11")
+nkar10 = document.getElementById("dv5_1")
+aj10 = document.getElementById("dv4_11")
+
+var arr10 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar10.style.backgroundImage = "url(img/1.jpg)"
+nkar10.style.backgroundSize = "cover"
+var l = 0
+
+aj10.onclick = function () {
+    l++
+    nkar10.style.backgroundImage = `url(img/${arr10[l]})`
+    if (l === 4) {
+        l = 0
+        nkar10.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax10.onclick = function () {
+    l--
+    nkar10.style.backgroundImage = `url(img/${arr10[l]})`
+    if (l < 0) {
+        l = 3
+        nkar10.style.backgroundImage = `url(img/${arr10[l]})`
+    }
+}
+dax12 = document.getElementById("dv3_12")
+nkar12 = document.getElementById("dv5_2")
+aj12 = document.getElementById("dv4_12")
+
+var arr12 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar12.style.backgroundImage = "url(img/1.jpg)"
+nkar12.style.backgroundSize = "cover"
+var l = 0
+
+aj12.onclick = function () {
+    l++
+    nkar12.style.backgroundImage = `url(img/${arr12[l]})`
+    if (l === 4) {
+        l = 0
+        nkar12.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax12.onclick = function () {
+    l--
+    nkar12.style.backgroundImage = `url(img/${arr12[l]})`
+    if (l < 0) {
+        l = 3
+        nkar12.style.backgroundImage = `url(img/${arr12[l]})`
+    }
+}
+
+dax13 = document.getElementById("dv3_13")
+nkar13 = document.getElementById("dv5_3")
+aj13 = document.getElementById("dv4_13")
+
+var arr13 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar13.style.backgroundImage = "url(img/1.jpg)"
+nkar13.style.backgroundSize = "cover"
+var l = 0
+
+aj13.onclick = function () {
+    l++
+    nkar13.style.backgroundImage = `url(img/${arr13[l]})`
+    if (l === 4) {
+        l = 0
+        nkar13.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax13.onclick = function () {
+    l--
+    nkar13.style.backgroundImage = `url(img/${arr13[l]})`
+    if (l < 0) {
+        l = 3
+        nkar13.style.backgroundImage = `url(img/${arr13[l]})`
+    }
+}
+dax14 = document.getElementById("dv3_14")
+nkar14 = document.getElementById("dv5_4")
+aj14 = document.getElementById("dv4_14")
+
+var arr14 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar14.style.backgroundImage = "url(img/1.jpg)"
+nkar14.style.backgroundSize = "cover"
+var l = 0
+
+aj14.onclick = function () {
+    l++
+    nkar14.style.backgroundImage = `url(img/${arr14[l]})`
+    if (l === 4) {
+        l = 0
+        nkar14.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax14.onclick = function () {
+    l--
+    nkar14.style.backgroundImage = `url(img/${arr14[l]})`
+    if (l < 0) {
+        l = 3
+        nkar14.style.backgroundImage = `url(img/${arr14[l]})`
+    }
+}
+dax15 = document.getElementById("dv3_15")
+nkar15 = document.getElementById("dv5_5")
+aj15 = document.getElementById("dv4_15")
+
+var arr15 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar15.style.backgroundImage = "url(img/1.jpg)"
+nkar15.style.backgroundSize = "cover"
+var l = 0
+
+aj15.onclick = function () {
+    l++
+    nkar15.style.backgroundImage = `url(img/${arr15[l]})`
+    if (l === 4) {
+        l = 0
+        nkar15.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax15.onclick = function () {
+    l--
+    nkar15.style.backgroundImage = `url(img/${arr15[l]})`
+    if (l < 0) {
+        l = 3
+        nkar15.style.backgroundImage = `url(img/${arr15[l]})`
+    }
+}
+dax16 = document.getElementById("dv3_16")
+nkar16 = document.getElementById("dv5_6")
+aj16 = document.getElementById("dv4_16")
+
+var arr16 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar16.style.backgroundImage = "url(img/1.jpg)"
+nkar16.style.backgroundSize = "cover"
+var l = 0
+
+aj16.onclick = function () {
+    l++
+    nkar16.style.backgroundImage = `url(img/${arr16[l]})`
+    if (l === 4) {
+        l = 0
+        nkar16.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax16.onclick = function () {
+    l--
+    nkar16.style.backgroundImage = `url(img/${arr16[l]})`
+    if (l < 0) {
+        l = 3
+        nkar16.style.backgroundImage = `url(img/${arr16[l]})`
+    }
+}
+dax17 = document.getElementById("dv3_17")
+nkar17 = document.getElementById("dv5_7")
+aj17 = document.getElementById("dv4_17")
+
+var arr17 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar17.style.backgroundImage = "url(img/1.jpg)"
+nkar17.style.backgroundSize = "cover"
+var l = 0
+
+aj17.onclick = function () {
+    l++
+    nkar17.style.backgroundImage = `url(img/${arr17[l]})`
+    if (l === 4) {
+        l = 0
+        nkar17.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax17.onclick = function () {
+    l--
+    nkar17.style.backgroundImage = `url(img/${arr17[l]})`
+    if (l < 0) {
+        l = 3
+        nkar17.style.backgroundImage = `url(img/${arr17[l]})`
+    }
+}
+dax18 = document.getElementById("dv3_18")
+nkar18 = document.getElementById("dv5_8")
+aj18 = document.getElementById("dv4_18")
+
+var arr18 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar18.style.backgroundImage = "url(img/1.jpg)"
+nkar18.style.backgroundSize = "cover"
+var l = 0
+
+aj18.onclick = function () {
+    l++
+    nkar18.style.backgroundImage = `url(img/${arr18[l]})`
+    if (l === 4) {
+        l = 0
+        nkar18.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax18.onclick = function () {
+    l--
+    nkar18.style.backgroundImage = `url(img/${arr18[l]})`
+    if (l < 0) {
+        l = 3
+        nkar18.style.backgroundImage = `url(img/${arr18[l]})`
+    }
+}
+dax19 = document.getElementById("dv3_19")
+nkar19 = document.getElementById("dv5_9")
+aj19 = document.getElementById("dv4_19")
+
+var arr19 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkar19.style.backgroundImage = "url(img/1.jpg)"
+nkar19.style.backgroundSize = "cover"
+var l = 0
+
+aj19.onclick = function () {
+    l++
+    nkar19.style.backgroundImage = `url(img/${arr19[l]})`
+    if (l === 4) {
+        l = 0
+        nkar19.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+dax19.onclick = function () {
+    l--
+    nkar19.style.backgroundImage = `url(img/${arr19[l]})`
+    if (l < 0) {
+        l = 3
+        nkar19.style.backgroundImage = `url(img/${arr19[l]})`
+    }
+}
+
+//poighy5puty054ytg0pq35j08yqy4jryh4w57yuy
+daxb = document.getElementById("dv22")
+nkarb = document.getElementById("dv44")
+ajb = document.getElementById("dv33")
+
+var arrb = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb.style.backgroundImage = "url(img/1.jpg)"
+nkarb.style.backgroundSize = "cover"
+var l = 0
+
+ajb.onclick = function () {
+    l++
+    nkarb.style.backgroundImage = `url(img/${arrb[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb.onclick = function () {
+    l--
+    nkarb.style.backgroundImage = `url(img/${arrb[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb.style.backgroundImage = `url(img/${arr[l]})`
+    }
+}
+daxb2 = document.getElementById("dv12_2")
+nkarb2 = document.getElementById("dv14_2")
+ajb2 = document.getElementById("dv13_2")
+
+var arrb2 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb2.style.backgroundImage = "url(img/1.jpg)"
+nkarb2.style.backgroundSize = "cover"
+var l = 0
+
+ajb2.onclick = function () {
+    l++
+    nkarb2.style.backgroundImage = `url(img/${arrb2[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb2.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb2.onclick = function () {
+    l--
+    nkarb2.style.backgroundImage = `url(img/${arrb2[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb2.style.backgroundImage = `url(img/${arrb2[l]})`
+    }
+}
+
+daxb3 = document.getElementById("dv12_3")
+nkarb3 = document.getElementById("dv14_3")
+ajb3 = document.getElementById("dv13_3")
+
+var arrb3 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb3.style.backgroundImage = "url(img/1.jpg)"
+nkarb3.style.backgroundSize = "cover"
+var l = 0
+
+ajb3.onclick = function () {
+    l++
+    nkarb3.style.backgroundImage = `url(img/${arrb3[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb3.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb3.onclick = function () {
+    l--
+    nkarb3.style.backgroundImage = `url(img/${arrb3[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb3.style.backgroundImage = `url(img/${arrb3[l]})`
+    }
+}
+daxb4 = document.getElementById("dv12_4")
+nkarb4 = document.getElementById("dv14_4")
+ajb4 = document.getElementById("dv13_4")
+
+var arrb4 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb4.style.backgroundImage = "url(img/1.jpg)"
+nkarb4.style.backgroundSize = "cover"
+var l = 0
+
+ajb4.onclick = function () {
+    l++
+    nkarb4.style.backgroundImage = `url(img/${arrb4[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb4.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb4.onclick = function () {
+    l--
+    nkarb4.style.backgroundImage = `url(img/${arrb4[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb4.style.backgroundImage = `url(img/${arrb4[l]})`
+    }
+}
+daxb5 = document.getElementById("dv12_5")
+nkarb5 = document.getElementById("dv14_5")
+ajb5 = document.getElementById("dv13_5")
+
+var arrb5 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb5.style.backgroundImage = "url(img/1.jpg)"
+nkarb5.style.backgroundSize = "cover"
+var l = 0
+
+ajb5.onclick = function () {
+    l++
+    nkarb5.style.backgroundImage = `url(img/${arrb5[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb5.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb5.onclick = function () {
+    l--
+    nkarb5.style.backgroundImage = `url(img/${arrb5[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb5.style.backgroundImage = `url(img/${arrb5[l]})`
+    }
+}
+daxb6 = document.getElementById("dv12_6")
+nkarb6 = document.getElementById("dv14_6")
+ajb6 = document.getElementById("dv13_6")
+
+var arrb6 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb6.style.backgroundImage = "url(img/1.jpg)"
+nkarb6.style.backgroundSize = "cover"
+var l = 0
+
+ajb6.onclick = function () {
+    l++
+    nkarb6.style.backgroundImage = `url(img/${arrb6[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb6.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb6.onclick = function () {
+    l--
+    nkarb6.style.backgroundImage = `url(img/${arrb6[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb6.style.backgroundImage = `url(img/${arrb6[l]})`
+    }
+}
+daxb7 = document.getElementById("dv12_7")
+nkarb7 = document.getElementById("dv14_7")
+ajb7 = document.getElementById("dv13_7")
+
+var arrb7 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb7.style.backgroundImage = "url(img/1.jpg)"
+nkarb7.style.backgroundSize = "cover"
+var l = 0
+
+ajb7.onclick = function () {
+    l++
+    nkarb7.style.backgroundImage = `url(img/${arrb7[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb7.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb7.onclick = function () {
+    l--
+    nkarb7.style.backgroundImage = `url(img/${arrb7[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb7.style.backgroundImage = `url(img/${arrb7[l]})`
+    }
+}
+daxb8 = document.getElementById("dv12_8")
+nkarb8 = document.getElementById("dv14_8")
+ajb8 = document.getElementById("dv13_8")
+
+var arrb8 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb8.style.backgroundImage = "url(img/1.jpg)"
+nkarb8.style.backgroundSize = "cover"
+var l = 0
+
+ajb8.onclick = function () {
+    l++
+    nkarb8.style.backgroundImage = `url(img/${arrb8[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb8.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb8.onclick = function () {
+    l--
+    nkarb8.style.backgroundImage = `url(img/${arrb8[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb8.style.backgroundImage = `url(img/${arrb8[l]})`
+    }
+}
+daxb9 = document.getElementById("dv12_9")
+nkarb9 = document.getElementById("dv14_9")
+ajb9 = document.getElementById("dv13_9")
+
+var arrb9 = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`]
+nkarb9.style.backgroundImage = "url(img/1.jpg)"
+nkarb9.style.backgroundSize = "cover"
+var l = 0
+
+ajb9.onclick = function () {
+    l++
+    nkarb9.style.backgroundImage = `url(img/${arrb9[l]})`
+    if (l === 4) {
+        l = 0
+        nkarb9.style.backgroundImage = `url(img/1.jpg)`
+    }
+}
+
+daxb9.onclick = function () {
+    l--
+    nkarb9.style.backgroundImage = `url(img/${arrb9[l]})`
+    if (l < 0) {
+        l = 3
+        nkarb9.style.backgroundImage = `url(img/${arrb9[l]})`
+    }
+};
